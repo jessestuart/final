1152af67f83d4efb1adde122addfa79b59f24b76
Jesse Stuart <jdstuart@icloud.com>
2015-02-13 18:12:20 -0500

    Formatting

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,9 +7,9 @@ grails.project.source.level = 1.7
 grails.project.target.level = 1.7
 
 grails.project.fork = [
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon: true],
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
@@ -19,7 +19,7 @@ grails.project.dependency.resolution = {
     log "error"
     checksums true
     legacyResolve false
-    
+
     def seleniumVersion = "2.27.0"
 
     repositories {
@@ -29,24 +29,24 @@ grails.project.dependency.resolution = {
         mavenLocal()
         grailsCentral()
         mavenCentral()
-		mavenRepo "http://repo.spring.io/milestone/"
+        mavenRepo "http://repo.spring.io/milestone/"
     }
 
     dependencies {
-		compile "com.asual.lesscss:lesscss-engine:1.4.2"
+        compile "com.asual.lesscss:lesscss-engine:1.4.2"
         runtime "postgresql:postgresql:9.1-901.jdbc4"
         test("org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion") {
-			exclude "xml-apis"
-		}
-		test "org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion"
-		test "org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion"
-		test "org.seleniumhq.selenium:selenium-support:$seleniumVersion"
-		test "org.gebish:geb-spock:0.9.2"
+            exclude "xml-apis"
+        }
+        test "org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion"
+        test "org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion"
+        test "org.seleniumhq.selenium:selenium-support:$seleniumVersion"
+        test "org.gebish:geb-spock:0.9.2"
     }
 
     plugins {
-		build ":tomcat:7.0.52.1"
-		
+        build ":tomcat:7.0.52.1"
+
         compile ":scaffolding:2.0.2"
         compile ':cache:1.1.1'
         compile ":fields:1.3"
@@ -62,12 +62,12 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:3.6.10.10"
         runtime ":database-migration:1.3.8"
         runtime ":jquery:1.11.0.2"
-		runtime ":jquery-ui:1.10.3"
+        runtime ":jquery-ui:1.10.3"
         runtime ":resources:1.2.7"
         runtime ":cache-headers:1.1.5"
         runtime ":cached-resources:1.1"
         runtime ":zipped-resources:1.0.1"
-        
-		test ":geb:0.9.2"
+
+        test ":geb:0.9.2"
     }
 }
